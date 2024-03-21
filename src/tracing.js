7037161829ad9feeda55c3c9ba4f331e5d9202ca
vinitaaher <vinitaaher.VA@gmail.com>
2024-03-21 14:55:45 +0530

    Added tracing code. Also change dashboard

--- src/tracing.js
+++ src/tracing.js
@@ -1,11 +1,9 @@
 
+    // Metrics
 import { DiagConsoleLogger, DiagLogLevel, diag, metrics } from '@opentelemetry/api';
 import {OTLPMetricExporter} from '@opentelemetry/exporter-metrics-otlp-http';
 import {MeterProvider,PeriodicExportingMetricReader,ConsoleMetricExporter, AggregationTemporality,View,InstrumentType} from '@opentelemetry/sdk-metrics';
- import { onFID, onLCP, onCLS} from 'web-vitals';
- import { Resource } from '@opentelemetry/resources';
-
-                  // Metrics
+import { onFID, onLCP, onCLS} from 'web-vitals';
 
    let interval;
    console.log('STARTING METRICS');
@@ -58,3 +56,91 @@ import {MeterProvider,PeriodicExportingMetricReader,ConsoleMetricExporter, Aggre
 
 
   // tracing
+import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-http';
+import { WebTracerProvider, BatchSpanProcessor, SimpleSpanProcessor, ConsoleSpanExporter } from '@opentelemetry/sdk-trace-web';
+import { ZoneContextManager } from '@opentelemetry/context-zone';
+import { Resource } from '@opentelemetry/resources';
+import { SemanticResourceAttributes } from '@opentelemetry/semantic-conventions';
+import { registerInstrumentations } from '@opentelemetry/instrumentation';
+import { getWebAutoInstrumentations } from '@opentelemetry/auto-instrumentations-web';
+import { WebVitalsInstrumentation } from './web-vitals-autoinstrumentation';
+import { DocumentLoadInstrumentation } from '@opentelemetry/instrumentation-document-load';
+import { FetchInstrumentation } from '@opentelemetry/instrumentation-fetch';
+import { UserInteractionInstrumentation } from '@opentelemetry/instrumentation-user-interaction';
+
+const tracingExporter = new OTLPTraceExporter({
+  serviceName: "auto-instrumentations-web",
+  url: "http://localhost:4318/v1/traces"
+});
+
+export const provider = new WebTracerProvider({
+  resource: new Resource({
+    [SemanticResourceAttributes.SERVICE_NAME]: 'browser_Instrumentation',
+    'user_agent.original': navigator.userAgent,
+    'browser.platform': navigator.userAgentData.platform,
+    'browser.brands': navigator.userAgentData.brands,
+    'browser.language': navigator.language,
+    'os.name': navigator.platform,
+    'host.name': 'localhost',
+    'browser.mobile': navigator.userAgent.includes('Mobi'),
+    'browser.touch_screen_enabled': navigator.maxTouchPoints > 0,
+    'browser.language': navigator.language,
+    'screen.width': window.screen.width,
+    'screen.height': window.screen.height,
+  }),
+});
+
+provider.addSpanProcessor(new BatchSpanProcessor(new ConsoleSpanExporter()));
+provider.addSpanProcessor(new SimpleSpanProcessor(tracingExporter));
+
+provider.register({
+  contextManager: new ZoneContextManager()
+});
+
+const addCustomAttributesToSpan = (span) => {
+  span.setAttribute('key1', 'abc');
+};
+
+const addCustomAttributesToResourceFetchSpan = (span) => {
+  span.setAttribute('key2', 'xyz');
+};
+
+registerInstrumentations({
+  instrumentations: [
+    WebVitalsInstrumentation,
+    getWebAutoInstrumentations({
+      "@opentelemetry/instrumentation-xml-http-request": {
+        enabled: true,
+      },
+      "@opentelemetry/instrumentation-document-load": {
+        enabled: true,
+        applyCustomAttributesOnSpan: {
+          documentLoad: addCustomAttributesToSpan,
+          resourceFetch: addCustomAttributesToResourceFetchSpan,
+        }
+      },
+      "@opentelemetry/instrumentation-user-interaction": {
+        enabled: true,
+        shouldPreventSpanCreation: (event, element, span) => {
+          span.setAttribute('key', 'xyz');
+        },
+        eventNames: [
+          "click",
+          "load",
+          "loadeddata",
+          "loadedmetadata",
+          "loadstart",
+          "error",
+        ]
+      },
+      '@opentelemetry/instrumentation-fetch': {
+        enabled: true,
+      }
+    })
+  ]
+});
+
+const fetchInstrumentation = new FetchInstrumentation();
+fetchInstrumentation.setTracerProvider(provider);
+fetch('http://localhost:63342/myapp/dist/index.html');
+fetch('http://localhost:63342/myapp/dist/main.js');
